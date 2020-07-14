9980526c9a40c6bdadec5f534f5cb40792e56e8c
unknown <sundp.kshetri@gmail.com>
2020-07-14 22:12:59 +0545

    Required folder created

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+const connectDB = require('./config/db');
+
+//Route files 
+const user= require('./src/routes/user')
 //Load env vars
 dotenv.config({path:'./config/config.env'});
 
+//Connect Db
+connectDB();
+
 const app = express();
+
+//Mount routers
+app.use('/api/user',user);
 
 const PORT = process.env.PORT || 5000;
 
